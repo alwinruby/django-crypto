045daab3df57eefcba0d170caca96c45624ce952
alwinruby <alwinsolanky@gmail.com>
2021-06-04 21:51:30 +0100

    Pulling price data

--- venv/projects/mysite/crypto/views.py
+++ venv/projects/mysite/crypto/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
+
+    # Grab Crypto Price data
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD,EUR,GBP")
+    price = json.loads(price_request.content)
+
+    #Grab Crypto News
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api': api})
+
+    return render(request, 'home.html', { 'api': api, 'price': price })
